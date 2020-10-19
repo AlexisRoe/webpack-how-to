1fd71ab6cd98bf2f488c013713d2dac93f5292f2
AlexisRoe <A.Roehrling@gmx.de>
2020-10-19 16:21:59 +0200

    Add picture to build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './dog.jpg';
 
 function component() {
     const element = document.createElement('div');
 
     // lodash included
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'Guys'], ' ');
     element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
